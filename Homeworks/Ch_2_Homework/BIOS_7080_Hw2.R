1615f2047b2c73a34137e9318c0d5d0f51619c31
Richard Davis <rdavis22@tulane.edu>
2017-02-04 04:02:50 +0000

    substitued the MSE of the residuals as the proper std for CI's

--- Homeworks/Ch_2_Homework/BIOS_7080_Hw2.R
+++ Homeworks/Ch_2_Homework/BIOS_7080_Hw2.R
@@ -17,18 +17,18 @@ time<-c(36.6, 39.2, 30.4, 37.1, 34.1, 17.5, 20.6, 18.7, 25.7, 22.0, 15.0, 10.4,
 prb1.tibble<-tibble(traffic_sig, time)
 
 #part c) ANOVA for the traffic data
-prb1.anova<-aov(time~traffic_sig, data=prb1.tibble)
+prb1.anva<-aov(time~traffic_sig, data=prb1.tibble)
 
 #part d&e) least square means, std, and 95% CI of each traffic stop type
 x_pre<-mean(time[1:5])
-std_pre<-sd(time[1:5])
-#*note, df=N-t for getting the t distribution
+std_pre<-summary(prb1.anova)[[1]][["Mean Sq"]][2] #MSE of residuls    #sd(time[1:5])
+#*note, df=N-t for getting the t distribution #use MSE of residual from ANOVA table
 CI_pre<-c(x_pre-std_pre*qt(0.975, df=12), x_pre+std_pre*qt(0.975, df=12))
 x_semi<-mean(time[6:10])
-std_semi<-sd(time[6:10])
+std_semi<-summary(prb1.anva)[[1]][["Mean Sq"]][2]#sd(time[6:10])
 CI_semi<-c(x_semi-std_semi*qt(0.975, df=12), x_semi+std_semi*qt(0.975, df=12))
 x_fully<-mean(time[11:15])
-std_fully<-sd(time[11:15])
+std_fully<-summary(prb1.anva)[[1]][["Mean Sq"]][2]#sd(time[11:15])
 CI_fully<-c(x_fully-std_fully*qt(0.975, df=12), x_fully+std_fully*qt(0.975, df=12))
 
 ####Problem 2####
@@ -46,23 +46,24 @@ prb2.anva<-aov(lettuce~treat.prb2, data=prb2.tibble)
 #part d&e) least square means, std, and 95% CI of each lettuce treat group
 #group trt=0
 x_0<-mean(lettuce[1:4])
-std_0<-sd(lettuce[1:4])
+#MSE of residuals
+std_0<-summary(prb2.anva)[[1]][["Mean Sq"]][2] #sd(lettuce[1:4])
 CI_0<-c(x_0-std_0*qt(0.975, df=15), x_0+std_0*qt(0.975, df=15))
 #group trt=50
 x_50<-mean(lettuce[5:8])
-std_50<-sd(lettuce[5:8])
+std_50<-summary(prb2.anva)[[1]][["Mean Sq"]][2] #sd(lettuce[5:8])
 CI_50<-c(x_50-std_50*qt(0.975, df=15), x_50+std_50*qt(0.975, df=15))
 #group trt=100
 x_100<-mean(lettuce[9:12])
-std_100<-sd(lettuce[9:12])
+std_100<-summary(prb2.anva)[[1]][["Mean Sq"]][2] #sd(lettuce[9:12])
 CI_100<-c(x_100-std_100*qt(0.975, df=15), x_100+std_100*qt(0.975, df=15))
 #group trt=50
 x_150<-mean(lettuce[13:16])
-std_150<-sd(lettuce[13:16])
+std_150<-summary(prb2.anva)[[1]][["Mean Sq"]][2]#sd(lettuce[13:16])
 CI_150<-c(x_150-std_150*qt(0.975, df=15), x_150+std_150*qt(0.975, df=15))
 #group trt=50
 x_200<-mean(lettuce[17:20])
-std_200<-sd(lettuce[17:20])
+std_200<-summary(prb2.anva)[[1]][["Mean Sq"]][2] #sd(lettuce[17:20])
 CI_200<-c(x_200-std_200*qt(0.975, df=15), x_200+std_200*qt(0.975, df=15))
 
 #part g) randomization of numbers 1-20
@@ -88,23 +89,24 @@ prb3.anva<-aov(serum_t3~treat.prb3, data=prb3.tibble)
 #part d&e) least square means, std, and 95% CI of each lettuce treat group
 #group trt=Premolt
 x_Premolt<-mean(serum_t3[1:5])
-std_Premolt<-sd(serum_t3[1:5])
+#MSE of residuals
+std_Premolt<-summary(prb3.anva)[[1]][["Mean Sq"]][2] #sd(serum_t3[1:5])
 CI_Premolt<-c(x_Premolt-std_Premolt*qt(0.975, df=20), x_Premolt+std_Premolt*qt(0.975, df=20))
 #group trt=Fasting
 x_Fast<-mean(serum_t3[6:10])
-std_Fast<-sd(serum_t3[6:10])
+std_Fast<-summary(prb3.anva)[[1]][["Mean Sq"]][2] #sd(serum_t3[6:10])
 CI_Fast<-c(x_Fast-std_Fast*qt(0.975, df=20), x_Fast+std_Fast*qt(0.975, df=20))
 #group trt=60 gram bran
 x_60<-mean(serum_t3[11:15])
-std_60<-sd(serum_t3[11:15])
+std_60<-summary(prb3.anva)[[1]][["Mean Sq"]][2] #sd(serum_t3[11:15])
 CI_60<-c(x_60-std_60*qt(0.975, df=20), x_60+std_60*qt(0.975, df=20))
 #group trt=80 gram bran
 x_80<-mean(serum_t3[16:20])
-std_80<-sd(serum_t3[16:20])
+std_80<-summary(prb3.anva)[[1]][["Mean Sq"]][2] #sd(serum_t3[16:20])
 CI_80<-c(x_80-std_80*qt(0.975, df=20), x_80+std_80*qt(0.975, df=20))
 #group trt=Laying mash
 x_LM<-mean(serum_t3[21:25])
-std_LM<-sd(serum_t3[21:25])
+std_LM<-summary(prb3.anva)[[1]][["Mean Sq"]][2] #sd(serum_t3[21:25])
 CI_LM<-c(x_LM-std_LM*qt(0.975, df=20), x_LM+std_LM*qt(0.975, df=20))
 
 #part g) randomization of numbers 1-20
@@ -131,19 +133,20 @@ prb4.anva<-aov(num_strat~treat.prb4, data=prb4.tibble)
 #part d&e) least square means, std, and 95% CI of each lettuce treat group
 #group trt=Control
 x_ctrl<-mean(num_strat[1:6])
-std_ctrl<-sd(num_strat[1:6])
+#MSE of residuals
+std_ctrl<-summary(prb4.anva)[[1]][["Mean Sq"]][2] #sd(num_strat[1:6])
 CI_ctrl<-c(x_ctrl-std_ctrl*qt(0.975, df=30), x_ctrl+std_ctrl*qt(0.975, df=30))
 #group trt=1979
 x_1979<-mean(num_strat[7:15])
-std_1979<-sd(num_strat[7:15])
+std_1979<-summary(prb4.anva)[[1]][["Mean Sq"]][2]#sd(num_strat[7:15])
 CI_1979<-c(x_1979-std_1979*qt(0.975, df=30), x_1979+std_1979*qt(0.975, df=30))
 #group trt=1980
 x_1980<-mean(num_strat[16:24])
-std_1980<-sd(num_strat[16:24])
+std_1980<-summary(prb4.anva)[[1]][["Mean Sq"]][2]#sd(num_strat[16:24])
 CI_1980<-c(x_1980-std_1980*qt(0.975, df=30), x_1980+std_1980*qt(0.975, df=30))
 #group trt=1981
 x_1981<-mean(num_strat[25:34])
-std_1981<-sd(num_strat[25:34])
+std_1981<-summary(prb4.anva)[[1]][["Mean Sq"]][2]#sd(num_strat[25:34])
 CI_1981<-c(x_1981-std_1981*qt(0.975, df=30), x_1981+std_1981*qt(0.975, df=30))
 
 #part g) randomization of numbers 1-20
@@ -167,23 +170,24 @@ prb5.anva<-aov(spect~treat.prb5, data=prb5.tibble)
 #part d&e) least square means, std, and 95% CI of each lettuce treat group
 #group trt=0.00
 x_0.00<-mean(spect[1:4])
-std_0.00<-sd(spect[1:4])
+#MSE of residuals
+std_0.00<-summary(prb5.anva)[[1]][["Mean Sq"]][2] #sd(spect[1:4])
 CI_0.00<-c(x_0.00-std_0.00*qt(0.975, df=7), x_0.00+std_0.00*qt(0.975, df=7))
 #group trt=0.05
 x_0.05<-mean(spect[5:6])
-std_0.05<-sd(spect[5:6])
+std_0.05<-summary(prb5.anva)[[1]][["Mean Sq"]][2]#sd(spect[5:6])
 CI_0.05<-c(x_0.05-std_0.05*qt(0.975, df=7), x_0.05+std_0.05*qt(0.975, df=7))
 #group trt=0.10
 x_0.10<-mean(spect[7:8])
-std_0.10<-sd(spect[7:8])
+std_0.10<-summary(prb5.anva)[[1]][["Mean Sq"]][2]#sd(spect[7:8])
 CI_0.10<-c(x_0.10-std_0.10*qt(0.975, df=7), x_0.10+std_0.10*qt(0.975, df=7))
 #group trt=0.20
 x_0.20<-mean(spect[9:10])
-std_0.20<-sd(spect[9:10])
+std_0.20<-summary(prb5.anva)[[1]][["Mean Sq"]][2]#sd(spect[9:10])
 CI_0.20<-c(x_0.20-std_0.20*qt(0.975, df=7), x_0.20+std_0.20*qt(0.975, df=7))
 #group trt=0.50
 x_0.50<-mean(spect[11:12])
-std_0.50<-sd(spect[11:12])
+std_0.50<-summary(prb5.anva)[[1]][["Mean Sq"]][2] #sd(spect[11:12])
 CI_0.50<-c(x_0.50-std_0.50*qt(0.975, df=7), x_0.50+std_0.50*qt(0.975, df=7))
 
 #part g) randomization of numbers 1-20
@@ -209,21 +213,22 @@ prb6.anva<-aov(serum_t3.prb6~treat.prb6, data=prb6.tibble)
 #part d&e) least square means, std, and 95% CI of each lettuce treat group
 #group trt=Premolt
 x_Premolt.prb6<-mean(serum_t3.prb6[1:5], na.rm=T)
-std_Premolt.prb6<-sd(serum_t3.prb6[1:5], na.rm=T)
+#MSE of residuals
+std_Premolt.prb6<-summary(prb6.anva)[[1]][["Mean Sq"]][2] #sd(serum_t3.prb6[1:5], na.rm=T)
 CI_Premolt.prb6<-c(x_Premolt.prb6-std_Premolt.prb6*qt(0.975, df=16), x_Premolt.prb6+std_Premolt.prb6*qt(0.975, df=16))
 #group trt=Fasting
 x_Fast.prb6<-mean(serum_t3.prb6[6:10], na.rm=T)
-std_Fast.prb6<-sd(serum_t3.prb6[6:10], na.rm=T)
+std_Fast.prb6<-summary(prb6.anva)[[1]][["Mean Sq"]][2]#sd(serum_t3.prb6[6:10], na.rm=T)
 CI_Fast.prb6<-c(x_Fast.prb6-std_Fast.prb6*qt(0.975, df=16), x_Fast.prb6+std_Fast.prb6*qt(0.975, df=16))
 #group trt=60 gram bran
 x_60.prb6<-mean(serum_t3.prb6[11:15], na.rm=T)
-std_60.prb6<-sd(serum_t3.prb6[11:15], na.rm=T)
+std_60.prb6<-summary(prb6.anva)[[1]][["Mean Sq"]][2]#sd(serum_t3.prb6[11:15], na.rm=T)
 CI_60.prb6<-c(x_60.prb6-std_60.prb6*qt(0.975, df=16), x_60.prb6+std_60.prb6*qt(0.975, df=16))
 #group trt=80 gram bran
 x_80.prb6<-mean(serum_t3.prb6[16:20], na.rm=T)
-std_80.prb6<-sd(serum_t3.prb6[16:20], na.rm=T)
+std_80.prb6<-summary(prb6.anva)[[1]][["Mean Sq"]][2]#sd(serum_t3.prb6[16:20], na.rm=T)
 CI_80.prb6<-c(x_80.prb6-std_80.prb6*qt(0.975, df=16), x_80.prb6+std_80.prb6*qt(0.975, df=16))
 #group trt=Laying mash
 x_LM.prb6<-mean(serum_t3.prb6[21:25], na.rm=T)
-std_LM.prb6<-sd(serum_t3.prb6[21:25], na.rm=T)
+std_LM.prb6<-summary(prb6.anva)[[1]][["Mean Sq"]][2] #sd(serum_t3.prb6[21:25], na.rm=T)
 CI_LM.prb6<-c(x_LM.prb6-std_LM.prb6*qt(0.975, df=16), x_LM.prb6+std_LM.prb6*qt(0.975, df=16))
