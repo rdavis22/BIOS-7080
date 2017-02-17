80d0acb2c6c973a20a56920bd15be3abbc6332e0
Richard Davis <rdavis22@tulane.edu>
2017-02-17 06:15:14 +0000

    completed ANOVA; added part 1b)

--- Exams/BIOS_7080_Exam_I.R
+++ Exams/BIOS_7080_Exam_I.R
@@ -16,3 +16,19 @@ if(!require(lmtest))
 ###Data Input###
 #caffeine dosage (mL)
 treat.prb1<-factor(c(rep(0, 10), rep(100, 10), rep(200, 10), rep(300, 10)))
+#number of finger taps after two hours
+finger_taps.prb1<-c(242, 245, 244, 248, 247, 248, 242, 244, 246, 242, 248, 246,
+                    245, 247, 248, 250, 247, 246, 243, 244, 246, 248, 250, 252,
+                    248, 250, 252, 248, 245, 250, 248, 250, 251, 251, 248, 251,
+                    252, 249, 253, 251)
+#combine the data into a tibble
+prb1.tibble<-tibble(treat.prb1, finger_taps.prb1)
+
+###Computation###
+##ANOVA##
+prb1.anva<-aov(finger_taps.prb1~treat.prb1, data = prb1.tibble)
+
+##b) assess model adequecy, fit, error normality, and error homoscedasticity
+##model fit
+#calculate residuals
+prb1.residuals<-residuals(prb1.anva)
