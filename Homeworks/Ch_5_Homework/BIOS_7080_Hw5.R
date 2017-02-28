1b7c0d7fa5e88f8f289dd4345164606ee9ee7f9b
Richard Davis <rdavis22@tulane.edu>
2017-02-28 17:22:34 +0000

    problem 5.1 done

--- Homeworks/Ch_5_Homework/BIOS_7080_Hw5.R
+++ Homeworks/Ch_5_Homework/BIOS_7080_Hw5.R
@@ -9,10 +9,96 @@ if(!require(lsmeans))
   install.packages("lsmeans")
 
 ####Problem E.g. 5.1####
+###Data Input###
+#indepent variable
 casting<-factor(c(rep(1, 10), rep(2, 10), rep(3, 10)))
+#casting variable (dependent variable)
 tens_stren<-c(88.0, 88.0, 94.8, 90.0, 93.0, 89.0, 86.0, 92.9, 89.0, 93.0, 85.9,
               88.6, 90.0, 87.1, 85.6, 86.0, 91.0, 89.6, 93.0, 87.5, 94.2, 91.5,
               92.0, 96.5, 95.6, 93.8, 92.5, 93.2, 96.2, 92.5)
-prb1.tibble<-tibble(casting, tens_stren)
+#dataframe for analysis
+prb5_1eg.tibble<-tibble(casting, tens_stren)
 
-prb1.anva<-aov()
+###ANOVA###
+prb5_1eg.aov<-aov(tens_stren~Error(casting))
+
+prb5_1eg.lmer<-lmer(tens_stren~1+(1|casting), data = prb1.tibble)
+prb5_1eg.anova<-anova(prb5_1eg.lmer)
+
+
+####Problem 5.1####
+###Data input###
+#beef animal sires that produced the offspring
+sires<-factor(c(rep(177, 8), rep(200, 8), rep(201, 8), rep(202, 8), rep(203, 8)))
+#birthweights of offspring
+bweights<-c(61, 100, 56, 113, 99, 103, 75, 62, 75, 102, 95, 103, 98, 115, 98, 
+            94, 58, 60, 60, 57, 57, 59, 54, 100, 57, 56, 67, 59, 58, 121, 101,
+            101, 59, 46, 120, 115, 115, 93, 105, 75)
+#dataframe for the data
+prb5_1.tibble<-tibble(sires, bweights)
+
+###Part a)###
+#classical method for random effects model
+prb5_1.aov<-aov(bweights~Error(sires), data=prb5_1.tibble)
+
+#mixed effects method for random effects model
+prb5_1.lmer<-lmer(bweights~1+(1|sires))
+prb5_1.anova<-anova(prb5_1.lmer)
+SSA.prb5_1<-5591
+SSW.prb5_1<-16233
+MSA.prb5_1<-1398
+MSW.prb5_1<-463.8
+
+###Part b)###
+#get the estimate for the F value
+Fval.prb5_1<-MSA.prb5_1/MSW.prb5_1
+
+#Number of treatments
+t.prb5_1<-length(levels(sires))
+#number of replications in each treatment
+r.prb5_1<-length(sires[sires=='177'])
+#total number of observations
+N.prb5_1<-length(sires)
+
+##point estimate and 90% CI for variance of error
+#point estimate for variance for progeny within sires
+sigmae2.prb5_1<-MSW.prb5_1
+#point estimate
+#chi-square variables for building 90% CI for sigma_error^2
+A.prb5_1<-qchisq(.95, N.prb5_1-t.prb5_1)
+B.prb5_1<-qchisq(.05, N.prb5_1-t.prb5_1)
+#90% CI for sigma_error^2
+CI90_sigmae2.prb5_1<-c(SSW.prb5_1/A.prb5_1, SSW.prb5_1/B.prb5_1)
+
+##point estimate and 90% CI for variance of treatment
+#point estimate for variance for sires
+sigmatau2.prb5_1<-(MSA.prb5_1-MSW.prb5_1)/r.prb5_1
+#critical values for sigma_tau^2
+#F upper critical value (F(0.05, t-1=4, t(r-1)=35)) for 90% CI for sigma_tau^2
+Fu.prb5_1<-qf(0.975, t.prb5_1-1, N.prb5_1-t.prb5_1)
+#F lower critical value (F(0.05, t-1=4, t(r-1)=35)) for 90% CI for sigma_tau^2
+Fl.prb5_1<-qf(0.025, t.prb5_1-1, N.prb5_1-t.prb5_1)
+##chi-square variables for building 90% CI for sigma_tau^2
+C.prb5_1<-qchisq(0.975, t.prb5_1-1)
+D.prb5_1<-qchisq(0.025, t.prb5_1-1)
+
+#90% CI for sigma_tau^2
+CI90_sigmatau2.prb5_1<-c(SSA.prb5_1*(1-Fu.prb5_1/Fval.prb5_1)/(r.prb5_1*C.prb5_1),
+                         SSA.prb5_1*(1-Fl.prb5_1/Fval.prb5_1)/(r.prb5_1*D.prb5_1))
+
+###Part c)###
+#Test the null hypothesis that sigma_tau^2=0
+#get the p-value
+pval.prb5_1<-pf(Fval.prb5_1, 4, 35, lower.tail = F)
+
+###Part d)###
+##Intraclass correlation coefficient with 90% CI
+#point estimate
+rhoI.prb5_1<-sigmatau2.prb5_1/(sigmatau2.prb5_1+sigmae2.prb5_1)
+
+#90% CI
+CI90_rhoI.prb5_1<-c((Fval.prb5_1-Fu.prb5_1)/(Fval.prb5_1+(r.prb5_1-1)*Fu.prb5_1),
+                    (Fval.prb5_1-Fl.prb5_1)/(Fval.prb5_1+(r.prb5_1-1)*Fl.prb5_1))
+
+
+####Problem 5.2####
