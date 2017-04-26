61d3853cd637bc4bd43460b34b66b2bf1aeb527e
Richard Davis <rdavis22@tulane.edu>
2017-04-27 00:02:46 +0100

    first commit: Problem 1 done

---
+++ Exams/BIOS_7080_Exam_III.R
@@ -0,0 +1,23 @@
+####BIOS 7080: Exam 3####
+###Load Design of Experiments Libraries###
+if(!require(tidyverse)){
+  install.packages("tidyverse"); library(tidyverse)}
+if(!require(nlme)){
+  install.packages("nlme"); library(nlme)}
+if(!require(car)){
+  install.packages("car"); library(car)}
+if(!require(MASS)){
+  install.packages("MASS"); library(MASS)}
+if(!require(lme4)){
+  install.packages("lme4"); library(lme4)}
+if(!require(lmerTest)){
+  install.packages("lmerTest"); library(lmerTest)}
+if(!require(lsmeans)){
+  install.packages("lsmeans"); library(lsmeans)}
+if(!require(daewr)){
+  install.packages("daewr"); library(daewr)}
+if(!require(multcomp)){
+  install.packages("multcomp"); library(multcomp)}
+
+####Problem 1####
+###Data Input###
